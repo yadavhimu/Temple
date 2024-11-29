a0399197c46bf78883ff7f6fcfd61a3c5926a764
yadavhimu <106873438+yadavhimu@users.noreply.github.com>
2024-11-29 17:01:29 +0530

    404 updated

--- src/pages/Chalisalakshmi.jsx
+++ src/pages/Chalisalakshmi.jsx
@@ -96,6 +96,215 @@ const Chalisalakshmi = () => {
           {" "}
           सुरनर मुनिजनहित सुखकारी॥
         </p>
+        <br />
+        <p className="mt-5 text-xl font-medium text-gray-600">
+          अद्भूत छटा मात तव बानी।
+        </p>
+        <p className="mt-5 text-xl font-medium text-gray-600">
+          सकलविश्व कीन्हो सुखखानी॥
+        </p>
+        <p className="mt-5 text-xl font-medium text-gray-600">
+          शांतिस्वभाव मृदुलतव भवानी।
+        </p>
+        <p className="mt-5 text-xl font-medium text-gray-600">
+          सकल विश्वकी हो सुखखानी॥
+        </p>
+        <br />
+        <p className="mt-5 text-xl font-medium text-gray-600">
+          महालक्ष्मी धन्य हो माई।
+        </p>
+        <p className="mt-5 text-xl font-medium text-gray-600">
+          पंच तत्व में सृष्टि रचाई॥
+        </p>
+        <p className="mt-5 text-xl font-medium text-gray-600">
+          जीव चराचर तुम उपजाए।
+        </p>
+        <p className="mt-5 text-xl font-medium text-gray-600">
+          पशु पक्षी नर नारी बनाए॥
+        </p>
+        <br />
+        <p className="mt-5 text-xl font-medium text-gray-600">
+          क्षितितल अगणित वृक्ष जमाए।
+        </p>
+        <p className="mt-5 text-xl font-medium text-gray-600">
+          अमितरंग फल फूल सुहाए॥
+        </p>
+        <p className="mt-5 text-xl font-medium text-gray-600">
+          छवि विलोक सुरमुनि नरनारी।
+        </p>
+        <p className="mt-5 text-xl font-medium text-gray-600">
+          करे सदा तव जय-जय कारी॥
+        </p>
+        <br />
+        <p className="mt-5 text-xl font-medium text-gray-600">
+          सुरपति औ नरपत सब ध्यावैं।
+        </p>
+        <p className="mt-5 text-xl font-medium text-gray-600">
+          तेरे सम्मुख शीश नवावैं॥
+        </p>
+        <p className="mt-5 text-xl font-medium text-gray-600">
+          चारहु वेदन तब यश गाया।
+        </p>
+        <p className="mt-5 text-xl font-medium text-gray-600">
+          महिमा अगम पार नहिं पाये॥
+        </p>
+        <br />
+        <p className="mt-5 text-xl font-medium text-gray-600">
+          जापर करहु मातु तुम दाया।
+        </p>
+        <p className="mt-5 text-xl font-medium text-gray-600">
+          सोइ जग में धन्य कहाया॥
+        </p>
+        <p className="mt-5 text-xl font-medium text-gray-600">
+          पल में राजाहि रंक बनाओ।
+        </p>
+        <p className="mt-5 text-xl font-medium text-gray-600">
+          रंक राव कर बिमल न लाओ॥
+        </p>
+        <br />
+        <p className="mt-5 text-xl font-medium text-gray-600">
+          जिन घर करहु माततुम बासा।
+        </p>
+        <p className="mt-5 text-xl font-medium text-gray-600">
+          उनका यश हो विश्व प्रकाशा॥
+        </p>
+        <p className="mt-5 text-xl font-medium text-gray-600">
+          जो ध्यावै से बहु सुख पावै।
+        </p>
+        <p className="mt-5 text-xl font-medium text-gray-600">
+          विमुख रहे हो दुख उठावै॥
+        </p>
+        <br />
+        <p className="mt-5 text-xl font-medium text-gray-600">
+          महालक्ष्मी जन सुख दाई।
+        </p>
+        <p className="mt-5 text-xl font-medium text-gray-600">
+          ध्याऊं तुमको शीश नवाई॥
+        </p>
+        <p className="mt-5 text-xl font-medium text-gray-600">
+          {" "}
+          निज जन जानीमोहीं अपनाओ।
+        </p>
+        <p className="mt-5 text-xl font-medium text-gray-600">
+          सुखसम्पति दे दुख नसाओ॥
+        </p>
+        <br />
+        <p className="mt-5 text-xl font-medium text-gray-600">
+          ॐ श्री-श्री जयसुखकी खानी।
+        </p>
+        <p className="mt-5 text-xl font-medium text-gray-600">
+          रिद्धिसिद्ध देउ मात जनजानी॥
+        </p>
+        <p className="mt-5 text-xl font-medium text-gray-600">
+          ॐह्रीं-ॐह्रीं सब व्याधिहटाओ।
+        </p>
+        <p className="mt-5 text-xl font-medium text-gray-600">
+          जनउन विमल दृष्टिदर्शाओ॥
+        </p>
+        <br />
+        <p className="mt-5 text-xl font-medium text-gray-600">
+          ॐक्लीं-ॐक्लीं शत्रुन क्षयकीजै।
+        </p>
+        <p className="mt-5 text-xl font-medium text-gray-600">
+          जनहित मात अभय वरदीजै॥
+        </p>
+        <p className="mt-5 text-xl font-medium text-gray-600">
+          ॐ जयजयति जयजननी।
+        </p>
+        <p className="mt-5 text-xl font-medium text-gray-600">
+          सकल काज भक्तन के सरनी॥
+        </p>
+        <br />
+        <p className="mt-5 text-xl font-medium text-gray-600">
+          ॐ नमो-नमो भवनिधि तारनी।
+        </p>
+        <p className="mt-5 text-xl font-medium text-gray-600">
+          तरणि भंवर से पार उतारनी॥
+        </p>
+        <p className="mt-5 text-xl font-medium text-gray-600">
+          सुनहु मात यह विनय हमारी।
+        </p>
+        <p className="mt-5 text-xl font-medium text-gray-600">
+          पुरवहु आशन करहु अबारी॥
+        </p>
+        <br />
+        <p className="mt-5 text-xl font-medium text-gray-600">
+          ऋणी दुखी जो तुमको ध्यावै।
+        </p>
+        <p className="mt-5 text-xl font-medium text-gray-600">
+          सो प्राणी सुख सम्पत्ति पावै॥
+        </p>
+        <p className="mt-5 text-xl font-medium text-gray-600">
+          रोग ग्रसित जो ध्यावै कोई।
+        </p>
+        <p className="mt-5 text-xl font-medium text-gray-600">
+          ताकी निर्मल काया होई॥
+        </p>
+        <br />
+        <p className="mt-5 text-xl font-medium text-gray-600">
+          विष्णु प्रिया जय-जय महारानी।
+        </p>
+        <p className="mt-5 text-xl font-medium text-gray-600">
+          महिमा अमित न जाय बखानी॥
+        </p>
+        <p className="mt-5 text-xl font-medium text-gray-600">
+          पुत्रहीन जो ध्यान लगावै।
+        </p>
+        <p className="mt-5 text-xl font-medium text-gray-600">
+          पाये सुत अतिहि हुलसावै॥
+        </p>
+        <br />
+        <p className="mt-5 text-xl font-medium text-gray-600">
+          त्राहि त्राहि शरणागत तेरी।
+        </p>
+        <p className="mt-5 text-xl font-medium text-gray-600">
+          करहु मात अब नेक न देरी॥
+        </p>
+        <p className="mt-5 text-xl font-medium text-gray-600">
+          आवहु मात विलम्ब न कीजै।
+        </p>
+        <p className="mt-5 text-xl font-medium text-gray-600">
+          हृदय निवास भक्त बर दीजै॥
+        </p>
+        <br />
+        <p className="mt-5 text-xl font-medium text-gray-600">
+          जानूं जप तप का नहिं भेवा।
+        </p>
+        <p className="mt-5 text-xl font-medium text-gray-600">
+          पार करो भवनिध वन खेवा॥
+        </p>
+        <p className="mt-5 text-xl font-medium text-gray-600">
+          बिनवों बार-बार कर जोरी।
+        </p>
+        <p className="mt-5 text-xl font-medium text-gray-600">
+          पूरण आशा करहु अब मोरी॥
+        </p>
+        <br />
+        <p className="mt-5 text-xl font-medium text-gray-600">
+          जानि दास मम संकट टारौ।
+        </p>
+        <p className="mt-5 text-xl font-medium text-gray-600">
+          सकल व्याधि से मोहिं उबारौ॥
+        </p>
+        <p className="mt-5 text-xl font-medium text-gray-600">
+          जो तव सुरति रहै लव लाई।
+        </p>
+        <p className="mt-5 text-xl font-medium text-gray-600">
+          सो जग पावै सुयश बड़ाई॥
+        </p>
+        <br />
+        <p className="mt-5 text-xl font-medium text-gray-600">
+          छायो यश तेरा संसारा।
+        </p>
+        <p className="mt-5 text-xl font-medium text-gray-600">
+          पावत शेष शम्भु नहिं पारा॥
+        </p>
+        <p className="mt-5 text-xl font-medium text-gray-600">
+          गोविंद निशदिन शरण तिहारी।
+        </p>
+        <p className="mt-5 text-xl font-medium text-gray-600">
+          करहु पूरण अभिलाष हमारी॥
+        </p>
         <br/>
       </div>
     </div>
